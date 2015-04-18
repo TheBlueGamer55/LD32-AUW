0c24716ff40718e1c7ffee3d943f03e39993a3f4
TheBlueGamer55 <endless.answers@gmail.com>
2015-04-17 22:21:59 -0700

    Added the Player class and basic movement & collision

---
+++ projects/core/src/com/swinestudios/unusualweapon/Player.java
@@ -0,0 +1,249 @@
+package com.swinestudios.unusualweapon;
+
+import org.mini2Dx.core.geom.Rectangle;
+import org.mini2Dx.core.graphics.Graphics;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
+public class Player{ 
+
+	public float x, y;
+	public float velX, velY;
+	public float accelX, accelY;
+
+	public final float frictionX = 0.4f;
+	public final float frictionY = 0.4f;
+
+	public final float moveSpeedX = 2.0f;
+	public final float moveSpeedY = 2.0f;
+
+	public final float maxSpeedX = 2.0f;
+	public final float maxSpeedY = 2.0f;
+
+	public boolean isActive;
+
+	public boolean facingRight, facingLeft;
+
+	public Rectangle hitbox;
+	public Gameplay level;
+	public String type;
+
+	//Controls/key bindings
+	public final int LEFT = Keys.LEFT;
+	public final int RIGHT = Keys.RIGHT;
+	public final int UP = Keys.UP;
+	public final int DOWN = Keys.DOWN;
+
+	public Player(float x, float y, Gameplay level){
+		this.x = x;
+		this.y = y;
+		hitbox = new Rectangle(x, y, 32, 32); 
+		velX = 0;
+		velY = 0;
+		accelX = 0;
+		accelY = 0;
+		isActive = false;
+		this.level = level;
+		type = "Player";
+	}
+
+	public void render(Graphics g){
+		g.setColor(Color.WHITE);
+		g.drawRect(x, y, 32, 32);
+	}
+
+	public void update(float delta){
+		accelX = 0;
+		accelY = 0;
+
+		//Move Left
+		if(Gdx.input.isKeyPressed(this.LEFT) && velX > -maxSpeedX){
+			accelX = -moveSpeedX;
+		}
+		//Move Right
+		if(Gdx.input.isKeyPressed(this.RIGHT) && velX < maxSpeedX){
+			accelX = moveSpeedX;
+		}
+		//Move Up
+		if(Gdx.input.isKeyPressed(this.UP) && velY > -maxSpeedY){
+			accelY = -moveSpeedY;
+		}
+		//Move Down
+		if(Gdx.input.isKeyPressed(this.DOWN) && velY < maxSpeedY){
+			accelY = moveSpeedY;
+		}
+
+		//Apply friction when not moving or when exceeding the max horizontal speed
+		if(Math.abs(velX) > maxSpeedX || !Gdx.input.isKeyPressed(this.LEFT) && !Gdx.input.isKeyPressed(this.RIGHT)){
+			friction(true, false);
+		}
+		//Apply friction when not moving or when exceeding the max vertical speed
+		if(Math.abs(velY) > maxSpeedY || !Gdx.input.isKeyPressed(this.UP) && !Gdx.input.isKeyPressed(this.DOWN)){
+			friction(false, true);
+		}
+
+		limitSpeed(true, true);
+		move();
+		hitbox.setX(this.x);
+		hitbox.setY(this.y);
+	}
+
+	/*
+	 * Checks if there is a collision if the player was at the given position.
+	 */
+	public boolean isColliding(Rectangle other, float x, float y){
+		if(other == this.hitbox){ //Make sure solid isn't stuck on itself
+			return false;
+		}
+		if(x < other.x + other.width && x + hitbox.width > other.x && y < other.y + other.height && y + hitbox.height > other.y){
+			return true;
+		}
+		return false;
+	}
+
+	/*
+	 * Helper method for checking whether there is a collision if the player moves at the given position
+	 */
+	public boolean collisionExistsAt(float x, float y){
+		for(int i = 0; i < level.solids.size(); i++){
+			Rectangle solid = level.solids.get(i);
+			if(isColliding(solid, x, y)){
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public void move(){
+		moveX();
+		moveY();
+	}
+
+	/*
+	 * Applies a friction force in the given axes by subtracting the respective velocity components
+	 * with the given friction components.
+	 */
+	public void friction(boolean horizontal, boolean vertical){
+		//if there is horizontal friction
+		if(horizontal){
+			if(velX > 0){
+				velX -= frictionX; //slow down
+				if(velX < 0){
+					velX = 0;
+				}
+			}
+			if(velX < 0){
+				velX += frictionX; //slow down
+				if(velX > 0){
+					velX = 0;
+				}
+			}
+		}
+		//if there is vertical friction
+		if(vertical){
+			if(velY > 0){
+				velY -= frictionY; //slow down
+				if(velY < 0){
+					velY = 0;
+				}
+			}
+			if(velY < 0){
+				velY += frictionY; //slow down
+				if(velY > 0){
+					velY = 0;
+				}
+			}
+		}
+	}
+
+	/*
+	 * Limits the speed of the player to a set maximum
+	 */
+	protected void limitSpeed(boolean horizontal, boolean vertical){
+		//If horizontal speed should be limited
+		if(horizontal){
+			if(Math.abs(velX) > maxSpeedX){
+				velX = maxSpeedX * Math.signum(velX);
+			}
+		}
+		//If vertical speed should be limited
+		if(vertical){
+			if(Math.abs(velY) > maxSpeedY){
+				velY = maxSpeedY * Math.signum(velY);
+			}
+		}
+	}
+
+	/*
+	 * Returns the current tile position of the player, given the specific tile dimensions
+	 */
+	public float getTileX(int tileSize){
+		return (int)(x / tileSize) * tileSize;
+	}
+
+	/*
+	 * Returns the current tile position of the player, given the specific tile dimensions
+	 */
+	public float getTileY(int tileSize){
+		return (int)(y / tileSize) * tileSize;
+	}
+
+	/*
+	 * Returns the distance between the player and the given target
+	 */
+	public float distanceTo(Rectangle target){
+		return ((float)Math.pow(Math.pow((target.y - this.y), 2.0) + Math.pow((target.x - this.x), 2.0), 0.5));
+	}
+
+	/*
+	 * Move horizontally in the direction of the x-velocity vector. If there is a collision in
+	 * this direction, step pixel by pixel up until the player hits the solid.
+	 */
+	public void moveX(){
+		for(int i = 0; i < level.solids.size(); i++){
+			Rectangle solid = level.solids.get(i);
+			if(isColliding(solid, x + velX, y)){
+				while(!isColliding(solid, x + Math.signum(velX), y)){
+					x += Math.signum(velX);
+				}
+				velX = 0;
+			}
+		}
+		x += velX;
+		velX += accelX;
+	}
+
+	/*
+	 * Move vertically in the direction of the y-velocity vector. If there is a collision in
+	 * this direction, step pixel by pixel up until the player hits the solid.
+	 */
+	public void moveY(){
+		for(int i = 0; i < level.solids.size(); i++){
+			Rectangle solid = level.solids.get(i);
+			if(isColliding(solid, x, y + velY)){
+				while(!isColliding(solid, x, y + Math.signum(velY))){
+					y += Math.signum(velY);
+				}
+				velY = 0;
+			}
+		}
+		y += velY;
+		velY += accelY;
+	}
+
+	/*
+	 * Sets up any images that the player may have. Necessary because images are flipped and have the origin
+	 * on the bottom-left by default.
+	 */
+	public void adjustSprite(Sprite... s){
+		for(int i = 0; i < s.length; i++){
+			s[i].setOrigin(0, 0);
+			s[i].flip(false, true);
+		}
+	}
+
+}
+
